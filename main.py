86b73604f6a1ba5f6e4fa0eada1d33f9506f75fd
Scosh <22227871+gitscosh@users.noreply.github.com>
2025-10-29 22:46:12 -0700

    print book text

---
+++ main.py
@@ -0,0 +1,8 @@
+def get_book_text(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read()
+    
+def main():
+    book_text = get_book_text('books/frankenstein.txt')
+    print(book_text)
+main()
