5892eb982524d661fda8c404b1356d5013b8778d
Scosh <22227871+gitscosh@users.noreply.github.com>
2025-10-30 22:00:09 -0700

    checkpoint

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
     
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
-    print(book_text)
+    text = get_book_text('books/frankenstein.txt')
+    get_num_words(text)
+
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text):
+    words = str.split(text)
+    print(f"Found {len(words)} total words")
