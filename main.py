b8dff16fb235e811b004fa63c64def18ae5bee93
Scosh <22227871+gitscosh@users.noreply.github.com>
2025-11-02 20:45:35 -0800

    counting and sorting characters

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from stats import get_num_words
+from stats import get_num_words, get_chars_dict, sorted_list
 
-def get_book_text(filepath):
-    with open(filepath, 'r', encoding='utf-8') as file:
-        return file.read()
-    
+
 def main():
-    text = get_book_text('books/frankenstein.txt')
-    get_num_words(text)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    sorted_chars = sorted_list(chars_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for entry in sorted_chars:
+        if entry["char"].isalpha():
+            char=entry['char']
+            num=entry['num']
+            print(f"{char}: {num}")
+    print("============= END ===============")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def get_num_words(text):
-    words = str.split(text)
-    print(f"Found {len(words)} total words")
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_num(entry):
+    return entry["num"]
+
+def sorted_list(letters):
+    char_list = [{'char': char, 'num': num} for char, num in letters.items()]
+    char_list.sort(reverse=True, key=get_num)
+    return char_list
+
